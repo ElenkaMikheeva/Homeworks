6dfdf223f47c6151dc5a557046c41a883c649ae3
Helena Veles <helena.veles@mail.ru>
2023-10-08 19:24:17 +0300

    Сделана тридцать четвертая задача

---
+++ Task34/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateRandomArray(size);
+PrintArray(myArray);
+Console.WriteLine(CounterEvenNumbers(myArray));
+
+int[] CreateRandomArray(int size)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100, 999);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("] ");
+}
+
+int CounterEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
