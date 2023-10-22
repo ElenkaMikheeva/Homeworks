beeb3643842cdb48a69e5819bcad32871b00ef3b
Helena Veles <helena.veles@mail.ru>
2023-10-22 22:32:58 +0300

    задача 64 готова

---
+++ Task64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+void ShowNumbers(int num1)
+{
+    if (num1 >= 1)
+    {
+        System.Console.Write($" {num1}, ");
+        ShowNumbers(num1 - 1);
+    }
+}
+Console.WriteLine("Введите N: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write($"N = {num1} -> ");
+ShowNumbers(num1);
+
+
+
+
+
---
+++ Task66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SumNumbers(int numN, int numM)
+{
+    if(numN == numM) return numN;
+    else if (numM > numN)
+    {
+        return numM + SumNumbers(numM - 1, numN);
+    }
+    else return numN + SumNumbers(numM, numN - 1);
+   
+}
+
+Console.WriteLine("Введите N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"M = {numM}; N = {numN} -> {SumNumbers(numN, numM)}");
